aee4d83946f8ad887ebef60244f37d62d3983b6f
vindictusinn <saniteeandduros@gmail.com>
2016-06-23 11:46:15 -0700

    Didn't even have the right assets

--- js/controllers/controllers.js
+++ js/controllers/controllers.js
@@ -8,12 +8,6 @@ ItemControllers.controller("ListController", ['$scope','$http',
 				setTimeout(function(){
 						WhatIsTheURL = window.location.pathname
 						if (WhatIsTheURL == '/search.html') {$('.mainiteminput').val(search).trigger('input');}
-						if (WhatIsTheURL == '/material/etc.html') {
-							function(){
-							$('.CategoryOneInput').val(CatOne).trigger('input');
-							$('.CategoryTwoInput').val(CatTwo).trigger('input');
-							}
-							}
 						if (WhatIsTheURL == '/weapon/bastard+sword') {$('tr').each(function(){$(this).children().last().hide()})}
 						if (WhatIsTheURL == '/weapon/phantom+daggers') {$('tr').each(function(){$(this).children().last().hide()})}
 						if (WhatIsTheURL == '/weapon/spellsword') {$('tr').each(function(){$(this).children().last().hide()})}
