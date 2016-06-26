bfbb49a8060559e3c3965ffe15fbea48e1c92934
vindictusinn <saniteeandduros@gmail.com>
2016-06-26 11:25:07 -0700

    Testing to see if this will work

---
+++ js/menu.js
@@ -0,0 +1,133 @@
+$('#cssmenu').html('<ul>'+
+	'<li><a href="/" class="home">Index</a></li>'+
+	'<li><a href="">Items</a>'+
+		'<ul>'+
+			'<li><a href="">Armor</a>'+
+				'<ul>'+
+					'<li><a href="/armor/heavy-armor">Heavy Armor</a>'+
+						'<ul>'+
+							'<li><a href="/armor/heavy/helm">Helm</a>'+
+							'<li><a href="/armor/heavy/tunic">Tunic</a>'+
+							'<li><a href="/armor/heavy/pants">Pants</a>'+
+							'<li><a href="/armor/heavy/boots">Boots</a>'+
+							'<li><a href="/armor/heavy/gloves">Gloves</a>'+
+						'</ul>'+
+					'</li>'+
+					'<li><a href="/armor/plate-armor">Plate Armor</a>'+
+						'<ul>'+
+							'<li><a href="/armor/plate/helm">Helm</a>'+
+							'<li><a href="/armor/plate/tunic">Tunic</a>'+
+							'<li><a href="/armor/plate/pants">Pants</a>'+
+							'<li><a href="/armor/plate/boots">Boots</a>'+
+							'<li><a href="/armor/plate/gloves">Gloves</a>'+
+						'</ul>'+
+					'</li>'+
+					'<li><a href="/armor/light-armor">Light Armor</a>'+
+						'<ul>'+
+							'<li><a href="/armor/light/helm">Helm</a>'+
+							'<li><a href="/armor/light/tunic">Tunic</a>'+
+							'<li><a href="/armor/light/pants">Pants</a>'+
+							'<li><a href="/armor/light/boots">Boots</a>'+
+							'<li><a href="/armor/light/gloves">Gloves</a>'+
+						'</ul>'+
+					'</li>'+
+					'<li><a href="/armor/cloth-armor">Cloth Armor</a>'+
+						'<ul>'+
+								'<li><a href="/armor/cloth/helm">Helm</a>'+
+								'<li><a href="/armor/cloth/tunic">Tunic</a>'+
+								'<li><a href="/armor/cloth/pants">Pants</a>'+
+								'<li><a href="/armor/cloth/boots">Boots</a>'+
+								'<li><a href="/armor/cloth/gloves">Gloves</a>'+
+						'</ul>'+
+					'</li>'+
+				'</ul>'+
+			'</li>'+
+			'<li><a href="">Weapons</a>'+
+				'<ul>'+
+					'<li><a href="/weapon/twin-swords">Twin Swords</a></li>'+
+					'<li><a href="/weapon/twin-spears">Twin Spears</a></li>'+
+					'<li><a href="/weapon/sword">Sword</a></li>'+
+					'<li><a href="/weapon/long-hammer">Long Hammer</a></li>'+
+					'<li><a href="/weapon/staff">Staff</a></li>'+
+					'<li><a href="/weapon/scythe">Scythe</a></li>'+
+					'<li><a href="/weapon/pillar">Pillar</a></li>'+
+					'<li><a href="/weapon/cestus">Cestus</a></li>'+
+					'<li><a href="/weapon/bow">Bow</a></li>'+
+					'<li><a href="/weapon/cross-gun">Cross Gun</a></li>'+
+					'<li><a href="/weapon/twin-chainblades">Twin Chainblades</a></li>'+
+					'<li><a href="/weapon/glaive">Glaive</a></li>'+
+					'<li><a href="/weapon/greatswords">Greatsword</a></li>'+
+					'<li><a href="/weapon/spellsword">Spellsword</a></li>'+
+					'<li><a href="/weapon/phantom-daggers">Phantom Daggers</a></li>'+
+					'<li><a href="/weapon/bastard+sword">Bastard Sword</a></li>'+
+				'</ul>'+
+			'</li>'+
+			'<li><a href="">Outfitters</a></li>'+
+			'<li><a href="">Accessories</a>'+
+				'<ul>'+
+					'<li><a href="/accessories/tail">Tail</a></li>'+
+					'<li><a href="">Rear</a>'+
+						'<ul>'+
+							'<li><a href="/accessories/rear/rear">Rear</a></li>'+
+							'<li><a href="/accessories/rear/wings">Wings</a></li>'+
+						'</ul>'+
+					'</li>'+
+				'</ul>'+
+			'</li>'+
+			'<li><a href="">Consumables</a></li>'+
+			'<li><a href="">Materials</a></li>'+
+			'<li><a href="">Battle</a>'+
+				'<ul>'+
+					'<li><a href="/battle/loot">Loot</a></li>'+
+					'<li><a href="/battle/story">Story</a></li>'+
+				'</ul>'+
+			'</li>'+
+			'<li><a href="">Other</a></li>'+
+		'</ul>'+
+	'</li>'+
+	'<li><a href="">Recipes</a>'+
+		'<ul>'+
+			'<li><a href="/expertise/tailoring">Tailoring</a></li>'+
+			'<li><a href="/expertise/goldsmithing">Goldsmithing</a></li>'+
+			'<li><a href="/expertise/weaponsmithing">Weaponsmithing</a></li>'+
+			'<li><a href="/expertise/armorsmithing">Armorsmithing</a></li>'+
+			'<li><a href="">Cooking</a></li>'+
+			'<li><a href="">NPC</a>'+
+				'<ul>'+
+					'<li><a href="">Clodagh</a></li>'+
+					'<li><a href="">Brynn</a></li>'+
+					'<li><a href="">Ferghus</a></li>'+
+					'<li><a href="">Kirstie</a></li>'+
+				'</ul>'+
+			'</li>'+
+		'</ul>'+
+	'</li>'+
+	'<li><a href="http://titles.vindictusinn.com">Titles</a></li>'+
+	'<li><a href="/">Skills</a>'+
+		'<ul>'+
+			'<li><a href="">Male</a>'+
+				'<ul>'+
+					'<li><a href="">Lann</a></li>'+
+					'<li><a href="">Karok</a></li>'+
+					'<li><a href="">Kai</a></li>'+
+					'<li><a href="">Hurk</a></li>'+
+					'<li><a href="">Sylas</a></li>'+
+				'</ul>'+
+			'</li>'+
+			'<li><a href="">Female</a>'+
+				'<ul>'+
+					'<li><a href="">Fiona</a></li>'+
+					'<li><a href="">Evie</a></li>'+
+					'<li><a href="">Vella</a></li>'+
+					'<li><a href="">Lynn</a></li>'+
+					'<li><a href="">Arisha</a></li>'+
+					'<li><a href="">Delia</a></li>'+
+				'</ul>'+
+			'</li>'+
+		'</ul>'+
+	'</li>'+
+	'<li><a href="/enemies">Enemies</a></li>'+
+	'<li><a href="/">Maps</a></li>'+
+	'<li><a href="/">Stories</a></li>'+
+	'<li><a href="/">NPCs</a></li>'+
+'</ul>')			
