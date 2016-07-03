59b322202daebd2b9267f13cd76f0e1ae1089b34
vindictusinn <saniteeandduros@gmail.com>
2016-07-02 20:39:09 -0700

    argh

--- js/OrbitControls.js
+++ js/OrbitControls.js
@@ -1,4 +1,4 @@
-RunOrbit = function(){/**
+/**
  * @author qiao / https://github.com/qiao
  * @author mrdoob / http://mrdoob.com
  * @author alteredq / http://alteredqualia.com/
@@ -401,4 +401,3 @@ THREE.OrbitControls = function ( object, domElement ) {
 };
 
 THREE.OrbitControls.prototype = Object.create( THREE.EventDispatcher.prototype );
-}
