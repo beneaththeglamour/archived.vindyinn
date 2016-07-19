f1e50eaa27fc057e6fa2639423fbba867960fd90
vindictusinn <saniteeandduros@gmail.com>
2016-07-18 22:49:12 -0700

    goodness

---
+++ js/controllers/controlbattles.js
@@ -0,0 +1,47 @@
+var ItemControllers = angular.module("ItemControllers", []);
+
+ItemControllers.controller("ListController", ['$scope','$http', 
+	function($scope, $http)
+		{    
+				$http.get('/json/QuestInfo.json').success (function(data){
+				$scope.ItemVariable = data;
+
+				setTimeout(function(){
+						
+				var url = "/js/descriptionsV2.js";
+							$.getScript( url, function() {
+								RunningDescriptions()
+							})
+				var menu = "/js/menu.js";
+							$.getScript( menu, function() {
+								MenuList()
+							})	
+	
+}, 200);
+			}); 
+		}]
+);
+
+ItemControllers.controller("DetailsController", ['$scope','$http','$routeParams',
+	 function($scope, $http, $routeParams)
+		{    
+				$http.get('/json/QuestInfo.json').success (function(data){
+				$scope.ItemVariable = data;
+					
+				$scope.whichItem = $routeParams.ItemID;
+				$scope.keyToDisplay = 'ItemClass';
+				setTimeout(function(){
+					var url = "/js/descriptionsV2.js";
+							$.getScript( url, function() {
+								RunningDescriptions()
+							})
+					var menu = "/js/menu.js";
+							$.getScript( menu, function() {
+								MenuList()
+							})	
+					$('#tabs').tabs();
+					$('#battletab').tabs();
+}, 200);
+			}); 
+		}]
+);
