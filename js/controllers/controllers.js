f3d5edbc051446a7cbcda1f32e023b786072b383
vindictusinn <saniteeandduros@gmail.com>
2016-07-21 14:50:30 -0700

    Correct

--- js/controllers/controllers.js
+++ js/controllers/controllers.js
@@ -384,15 +384,15 @@ ItemControllers.controller("DetailsController", ['$scope','$http','$routeParams'
 							$('.Expert'+ExpertMatID).val(ExpertMatID).trigger('input');
 							$('.ExpertTailor'+ExpertMatID).val('sewing_'+ExpertMatID).trigger('input');
 						})
-					function sformat( s ) {
-						  var fm = [
-											Math.floor(Math.floor(Math.floor(s/60)/60)/24)%24,      //DAYS
-											Math.floor(Math.floor(s/60)/60)%60,                          //HOURS
-											Math.floor(s/60)%60,                                                //MINUTES
-											s%60                                                                      //SECONDS
-									  ];
-						  return $.map(fm,function(v,i) { return ( (v < 10) ? '0' : '' ) + v; }).join( ':' );
-					}
+					    function sformat(s) {
+							  var fm = [
+									Math.floor(s / 60 / 60 / 24), // DAYS
+									Math.floor(s / 60 / 60) % 24, // HOURS
+									Math.floor(s / 60) % 60, // MINUTES
+									s % 60 // SECONDS
+							  ];
+							  return $.map(fm, function(v, i) { return ((v < 10) ? '0' : '') + v; }).join(':');
+						}
 					ConvertSeconds = $('.converttime').attr('value')
 					var sf = sformat( ConvertSeconds );  
 					$('.converttime').html(sf)
