53f026a7d22d228608bbc58706f705d6bd2aa1d2
vindictusinn <saniteeandduros@gmail.com>
2016-07-21 21:38:37 -0700

    No

--- js/controllers/controllers.js
+++ js/controllers/controllers.js
@@ -363,8 +363,8 @@ ItemControllers.controller("DetailsController", ['$scope','$http','$routeParams'
 					$('.iteminput').val(finalreplace).trigger('input');
 					$('.mainiteminput').val(ItemClass).trigger('input');
 					ThisIsStupod = $('#mainitem').attr('value')
-					$scope.mainitemclass = function (mat) { 	return mat.ItemClass === ThisIsStupod	
-					console.log('Yes You can run')};
+					$scope.mainitemclass = function (mat) { 	return mat.ItemClass === ThisIsStupod	;
+					console.log('Yes You can run') };
 					console.log('I sweartoGod')
 					$('.UsedInValue').each(function(){
 							console.log('I ran right Mommy?')
