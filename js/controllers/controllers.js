dd3eeb8c43228d3fd28054c3c0b1788bb0d473a3
vindictusinn <saniteeandduros@gmail.com>
2016-07-21 22:43:08 -0700

    Better

--- js/controllers/controllers.js
+++ js/controllers/controllers.js
@@ -348,7 +348,7 @@ ItemControllers.controller("DetailsController", ['$scope','$http','$routeParams'
 				$scope.whichItem = $routeParams.ItemID;
 				$scope.keyToDisplay = 'ItemClass';
 				setTimeout(function(){
-					var limitStep = 15;
+					var limitStep = 10;
 					$scope.limit = limitStep;
 					$scope.incrementLimit = function() {
 						$scope.limit += limitStep;
