8c5c995556a5358f41d61b3dbdc0ad5fcbaad294
vindictusinn <saniteeandduros@gmail.com>
2016-07-29 01:36:38 -0700

    Hey

--- js/controllers/controlskill.js
+++ js/controllers/controlskill.js
@@ -154,10 +154,9 @@ ItemControllers.controller("DetailsController", ['$scope','$http','$routeParams'
 											addap = parseInt($(this).attr('value'))
 											totalap = totalap + addap
 										} )
+										$('.skillid').append('<tfoot class="total"><tr><td></td><td></td><td class="centercell">Total</td><td class="centercell">' + totalap + '</td></tr><tfoot>')
 										console.log('DataTable loaded.')
 										$('.skillid').DataTable();
-										$('.skillid').append('<tfoot class="total"><tr><td></td><td></td><td class="centercell">Total</td><td class="centercell">' + totalap + '</td></tr><tfoot>')
-
 									})	
 							})	
 					var menu = "/js/menu.js";
