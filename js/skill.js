94b2613e17d0d8aef0b2d3612ce89af9926fc6f7
vindictusinn <saniteeandduros@gmail.com>
2016-07-29 12:17:32 -0700

    Installing disqus

--- js/skill.js
+++ js/skill.js
@@ -1,4 +1,4 @@
-var ItemApp = angular.module('ItemApp', ['ngRoute','ItemControllers']);
+var ItemApp = angular.module('ItemApp', ['ngRoute','ItemControllers','ngDisqus']);
 replacestart = $('#valueitem').attr('value');
 
 ItemApp.config(['$locationProvider', '$routeProvider', function($locationProvider, $routeProvider) {
@@ -16,3 +16,7 @@ ItemApp.config(['$locationProvider', '$routeProvider', function($locationProvide
     redirectTo: '/list'
   });
 }]);
+
+ ItemApp.config(function($disqusProvider){
+      $disqusProvider.setShortname(shortname);
+   });
