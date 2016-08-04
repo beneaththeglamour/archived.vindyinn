056d65b1c1258f4c8a0862dd85ebcdeb588f4a67
vindictusinn <saniteeandduros@gmail.com>
2016-08-04 00:41:44 -0700

    putting comments back

--- js/config/items.js
+++ js/config/items.js
@@ -88,3 +88,7 @@ ItemApp.config(['$locationProvider', '$routeProvider', function($locationProvide
   });
 }]);
 
+ItemApp.config(function($disqusProvider){
+	 shortname = 'vindictusinndb'
+      $disqusProvider.setShortname(shortname);
+   });
