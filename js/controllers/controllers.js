dd89a88827b078ba65be54b935dcb62c83e04df5
vindictusinn <saniteeandduros@gmail.com>
2016-08-04 00:12:26 -0700

    Alright adding disqus links

--- js/controllers/controllers.js
+++ js/controllers/controllers.js
@@ -887,8 +887,18 @@ ItemControllers.controller("DetailsController", ['$scope','$http','$routeParams'
 							})		
 					$.getScript( "/js/modelviewer.js", function() {
 								SetUpViewer()
-							})		
+							})								
 					$('#tabs').tabs();
+					var disqus_config = function () {
+						PageID = $('.itemdetail').attr('value') // Replace PAGE_IDENTIFIER with your page's unique identifier variable
+						this.page.identifier = PageID // Replace PAGE_IDENTIFIER with your page's unique identifier variable
+					};
+					(function() { // DON'T EDIT BELOW THIS LINE
+						var d = document, s = d.createElement('script');
+						s.src = '//vindictusinndb.disqus.com/embed.js';
+						s.setAttribute('data-timestamp', +new Date());
+						(d.head || d.body).appendChild(s);
+					})();
 }, 500);
 			}); 
 		}]
