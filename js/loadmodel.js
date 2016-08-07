b4fa90a99c9af8819dc0043fe36888fee50662e0
vindictusinn <saniteeandduros@gmail.com>
2016-08-06 18:26:00 -0700

    here we go

--- js/loadmodel.js
+++ js/loadmodel.js
@@ -30,7 +30,7 @@ LoadModel = function() {
 			texture.wrapT = THREE.RepeatWrapping;
 			texture.repeat.set( 1, 1 );
         material = new THREE.MeshFaceMaterial([
-			new THREE.MeshPhongMaterial( { map: texture2, shininess:100, specular:0xffffff  }),
+			new THREE.MeshPhongMaterial( { map: texture, shininess:100, specular:0xffffff  }),
 		]);
 	}
 }
