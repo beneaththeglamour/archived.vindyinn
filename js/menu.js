2a31e469ed16360d1a3f20cc71fd231195192d0b
vindictusinn <saniteeandduros@gmail.com>
2016-08-13 11:53:57 -0700

    Adding new menu

--- js/menu.js
+++ js/menu.js
@@ -263,13 +263,26 @@ $.fn.menumaker = function(options) {
 
         if (settings.sticky === true) cssmenu.css('position', 'fixed');
 
+        resizeFix = function() {
+          if ($( window ).width() > 768) {
+            cssmenu.find('ul').show();
+          }
+
+          if ($(window).width() <= 768) {
+            cssmenu.find('ul').hide().removeClass('open');
+          }
+        };
+        resizeFix();
+        return $(window).on('resize', resizeFix);
+
       });
   };
   
   $("#cssmenu").menumaker({
-   title: "Menu",
-   format: "multitoggle"
-});
+    title: "Navigation",
+    format: "multitoggle"
+	});
+
 console.log('CSSMenu Up')
 
 }
