b5554f324d78e38508cfb79b3f891cdf3b836b2f
vindictusinn <saniteeandduros@gmail.com>
2016-08-15 16:51:26 -0700

    This should be a song

--- js/jplayer/myplaylist.js
+++ js/jplayer/myplaylist.js
@@ -1,13 +1,12 @@
 var myPlaylist = [
 
     {
-        mp3:'mix/1.mp3',
-        oga:'mix/1.ogg',
-        title:'Sample',
-        artist:'Sample',
+        mp3:'https://a.tumblr.com/tumblr_nxuf1jH4S01rvumgmo1.mp3',
+        title:'Tieve&apos;s Theme',
+        artist:'Vindictus',
         rating:4,
-        duration:'0:30',
-        cover:'mix/1.png'
+        duration:'1:59',
+        cover:'http://images6.fanpop.com/image/photos/32400000/Morrighan-and-the-Crimson-Blades-vindictus-32467769-1000-499.jpg'
     },
 	 {
         mp3:'mix/1.mp3',
