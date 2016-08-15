4da12ce50aae9e72aac2fff145440b45ca659837
vindictusinn <saniteeandduros@gmail.com>
2016-08-15 14:10:38 -0700

    Try another song

--- js/jplayer/myplaylist.js
+++ js/jplayer/myplaylist.js
@@ -11,4 +11,15 @@ var myPlaylist = [
         duration:'0:30',
         cover:'mix/1.png'
     }
+	 {
+        mp3:'mix/1.mp3',
+        oga:'mix/1.ogg',
+        title:'Next One',
+        artist:'Sample',
+        rating:4,
+        buy:'#',
+        price:'0.99',
+        duration:'0:30',
+        cover:'mix/1.png'
+    }
 ];
