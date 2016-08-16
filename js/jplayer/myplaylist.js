1120f42cb0d239e2d15cb49718b7c58b058cde70
vindictusinn <saniteeandduros@gmail.com>
2016-08-16 11:05:47 -0700

    Updating playlist

--- js/jplayer/myplaylist.js
+++ js/jplayer/myplaylist.js
@@ -66,7 +66,7 @@ var myPlaylist = [
     },
 	 {
         mp3:'  ',
-        title:'bgm_s3_braha',
+        title:'Braha',
         artist:'Vindictus',
         rating:5,
         duration:'2:21',
@@ -82,7 +82,7 @@ var myPlaylist = [
     },
 	 {
         mp3:'  ',
-        title:'bgm_ep8_ahglan',
+        title:'Ahglan',
         artist:'Vindictus',
         rating:5,
         duration:'2:18',
@@ -126,6 +126,470 @@ var myPlaylist = [
         artist:'Vindictus',
         rating:5,
         duration:'2:12',
+        cover:'  ' 
+    },
+	 {
+        mp3:'   ',
+        title:'Eochaid',
+        artist:'Vindictus',
+        rating:5,
+        duration:'2:11',
+        cover:'  ' 
+    },
+	 {
+        mp3:'   ',
+        title:'bgm_s3_woodenelf',
+        artist:'Vindictus',
+        rating:5,
+        duration:'2:11',
+        cover:'  ' 
+    },
+	 {
+        mp3:'   ',
+        title:'bgm_boss_ruins',
+        artist:'Vindictus',
+        rating:5,
+        duration:'2:11',
+        cover:'  ' 
+    },
+	 {
+        mp3:'   ',
+        title:'Regina',
+        artist:'Vindictus',
+        rating:5,
+        duration:'2:11',
+        cover:'  ' 
+    },
+	 {
+        mp3:'   ',
+        title:'bgm_mboss_gnoll',
+        artist:'Vindictus',
+        rating:5,
+        duration:'2:11',
+        cover:'  ' 
+    },
+	 {
+        mp3:'   ',
+        title:'bgm_mboss_gnoll2',
+        artist:'Vindictus',
+        rating:5,
+        duration:'2:11',
+        cover:'  ' 
+    },
+	 {
+        mp3:'   ',
+        title:'bgm_mboss_kobold2',
+        artist:'Vindictus',
+        rating:5,
+        duration:'2:10',
+        cover:'  ' 
+    },
+	 {
+        mp3:'   ',
+        title:'bgm_mboss_troll',
+        artist:'Vindictus',
+        rating:5,
+        duration:'2:09',
+        cover:'  ' 
+    },
+	 {
+        mp3:'   ',
+        title:'Lugh Lamhfada',
+        artist:'Vindictus',
+        rating:5,
+        duration:'2:09',
+        cover:'  ' 
+    },
+	 {
+        mp3:'   ',
+        title:'bgm_pvp_p03',
+        artist:'Vindictus',
+        rating:5,
+        duration:'2:09',
+        cover:'  ' 
+    },
+	 {
+        mp3:'   ',
+        title:'Ancient Glas',
+        artist:'Vindictus',
+        rating:5,
+        duration:'2:09',
+        cover:'  ' 
+    },
+	 {
+        mp3:'   ',
+        title:'bgm_mboss_yeti',
+        artist:'Vindictus',
+        rating:5,
+        duration:'2:08',
+        cover:'  ' 
+    },
+	 {
+        mp3:'   ',
+        title:'bgm_s2ep4_1_theothers',
+        artist:'Vindictus',
+        rating:5,
+        duration:'2:08',
+        cover:'  ' 
+    },
+	 {
+        mp3:'   ',
+        title:'bgm_kadan_start',
+        artist:'Vindictus',
+        rating:5,
+        duration:'2:07',
+        cover:'  ' 
+    },
+	 {
+        mp3:'   ',
+        title:'bgm_quest_kalish',
+        artist:'Vindictus',
+        rating:5,
+        duration:'2:07',
+        cover:'  ' 
+    },
+	 {
+        mp3:'   ',
+        title:'bgm_s2ep1',
+        artist:'Vindictus',
+        rating:5,
+        duration:'2:07',
+        cover:'  ' 
+    },
+	 {
+        mp3:'   ',
+        title:'bgm_ep8_glasgavelen',
+        artist:'Vindictus',
+        rating:5,
+        duration:'2:06',
+        cover:'  ' 
+    },
+	 {
+        mp3:'   ',
+        title:'bgm_s2ep2_guardian',
+        artist:'Vindictus',
+        rating:5,
+        duration:'2:06',
+        cover:'  ' 
+    },
+	 {
+        mp3:'   ',
+        title:'bgm_mboss_ogre',
+        artist:'Vindictus',
+        rating:5,
+        duration:'2:05',
+        cover:'  ' 
+    },
+	 {
+        mp3:'   ',
+        title:'bgm_mboss_vampire',
+        artist:'Vindictus',
+        rating:5,
+        duration:'2:05',
+        cover:'  ' 
+    },
+	 {
+        mp3:'   ',
+        title:'Princess Iset',
+        artist:'Vindictus',
+        rating:5,
+        duration:'2:05',
+        cover:'  ' 
+    },
+	 {
+        mp3:'   ',
+        title:'Bark No.1',
+        artist:'Vindictus',
+        rating:5,
+        duration:'2:04',
+        cover:'  ' 
+    },
+	 {
+        mp3:'   ',
+        title:'Muir',
+        artist:'Vindictus',
+        rating:5,
+        duration:'2:04',
+        cover:'  ' 
+    },
+	 {
+        mp3:'   ',
+        title:'Waiting PvP',
+        artist:'Vindictus',
+        rating:5,
+        duration:'2:03',
+        cover:'  ' 
+    },
+	 {
+        mp3:'   ',
+        title:'Ingkells',
+        artist:'Vindictus',
+        rating:5,
+        duration:'2:03',
+        cover:'  ' 
+    },
+	 {
+        mp3:'   ',
+        title:'Cromm Cruaich',
+        artist:'Vindictus',
+        rating:5,
+        duration:'2:03',
+        cover:'  ' 
+    },
+	 {
+        mp3:'   ',
+        title:'bgm_s2ep2_grimreaper',
+        artist:'Vindictus',
+        rating:5,
+        duration:'2:03',
+        cover:'  ' 
+    },
+	 {
+        mp3:'   ',
+        title:'bgm_s3ep5_morbus',
+        artist:'Vindictus',
+        rating:5,
+        duration:'2:01',
+        cover:'  ' 
+    },
+	 {
+        mp3:'   ',
+        title:'bgm_s2ep3_gremlin2',
+        artist:'Vindictus',
+        rating:5,
+        duration:'2:01',
+        cover:'  ' 
+    },
+	 {
+        mp3:'   ',
+        title:'Juggernaut',
+        artist:'Vindictus',
+        rating:5,
+        duration:'2:01',
+        cover:'  ' 
+    },
+	 {
+        mp3:'   ',
+        title:'Zecallion',
+        artist:'Vindictus',
+        rating:5,
+        duration:'2:00',
+        cover:'  ' 
+    },
+	 {
+        mp3:'   ',
+        title:'bgm_s2ep3_puppet',
+        artist:'Vindictus',
+        rating:5,
+        duration:'2:00',
+        cover:'  ' 
+    },
+	 {
+        mp3:'   ',
+        title:'bgm_mboss_kobold',
+        artist:'Vindictus',
+        rating:5,
+        duration:'1:59',
+        cover:'  ' 
+    },
+	 {
+        mp3:'   ',
+        title:'Succubus Queen',
+        artist:'Vindictus',
+        rating:5,
+        duration:'1:58',
+        cover:'  ' 
+    },
+	 {
+        mp3:'   ',
+        title:'bgm_boss_icecave',
+        artist:'Vindictus',
+        rating:5,
+        duration:'1:57',
+        cover:'  ' 
+    },
+	 {
+        mp3:'   ',
+        title:'Havan',
+        artist:'Vindictus',
+        rating:5,
+        duration:'1:57',
+        cover:'  ' 
+    },
+	 {
+        mp3:'   ',
+        title:'Cromm Cruaich (Final Phase)',
+        artist:'Vindictus',
+        rating:5,
+        duration:'1:57',
+        cover:'  ' 
+    },
+	 {
+        mp3:'   ',
+        title:'bgm_ep9_common',
+        artist:'Vindictus',
+        rating:5,
+        duration:'1:55',
+        cover:'  ' 
+    },
+	 {
+        mp3:'   ',
+        title:'Colru',
+        artist:'Vindictus',
+        rating:5,
+        duration:'1:54',
+        cover:'  ' 
+    },
+	 {
+        mp3:'   ',
+        title:'bgm_rochest_01',
+        artist:'Vindictus',
+        rating:5,
+        duration:'1:50',
+        cover:'  ' 
+    },
+	 {
+        mp3:'   ',
+        title:'bgm_mboss_goblin2',
+        artist:'Vindictus',
+        rating:5,
+        duration:'1:48',
+        cover:'  ' 
+    },
+	 {
+        mp3:'   ',
+        title:'bgm_ep8_storyingkells',
+        artist:'Vindictus',
+        rating:5,
+        duration:'1:40',
+        cover:'  ' 
+    },
+	 {
+        mp3:'   ',
+        title:'Charles&apos; Race',
+        artist:'Vindictus',
+        rating:5,
+        duration:'1:17',
+        cover:'  ' 
+    },
+	 {
+        mp3:'   ',
+        title:'Charles&apos Expedition',
+        artist:'Vindictus',
+        rating:5,
+        duration:'1:13',
+        cover:'  ' 
+    },
+	 {
+        mp3:'   ',
+        title:'bgm_quest_success_03',
+        artist:'Vindictus',
+        rating:5,
+        duration:'1:07',
+        cover:'  ' 
+    },
+	 {
+        mp3:'   ',
+        title:'bgm_pvp_arena_battle',
+        artist:'Vindictus',
+        rating:5,
+        duration:'1:06',
+        cover:'  ' 
+    },
+	 {
+        mp3:'   ',
+        title:'bgm_ep8_talkingkells',
+        artist:'Vindictus',
+        rating:5,
+        duration:'1:03',
+        cover:'  ' 
+    },
+	 {
+        mp3:'   ',
+        title:'bgm_colhen_inn_sereuha',
+        artist:'Vindictus',
+        rating:5,
+        duration:'1:01',
+        cover:'  ' 
+    },
+	 {
+        mp3:'   ',
+        title:'Succubus Queen&apos;s Nightmare',
+        artist:'Vindictus',
+        rating:5,
+        duration:'0:39',
+        cover:'  ' 
+    },
+	 {
+        mp3:'   ',
+        title:'bgm_dragon_legend',
+        artist:'Vindictus',
+        rating:5,
+        duration:'0:36',
+        cover:'  ' 
+    },
+	 {
+        mp3:'   ',
+        title:'bgm_ep7_knight',
+        artist:'Vindictus',
+        rating:5,
+        duration:'0:35',
+        cover:'  ' 
+    },
+	 {
+        mp3:'   ',
+        title:'bgm_rochest_dormitory',
+        artist:'Vindictus',
+        rating:5,
+        duration:'0:33',
+        cover:'  ' 
+    },
+	 {
+        mp3:'   ',
+        title:'bgm_exp1_memory',
+        artist:'Vindictus',
+        rating:5,
+        duration:'0:28',
+        cover:'  ' 
+    },
+	 {
+        mp3:'   ',
+        title:'bgm_exp2_memory',
+        artist:'Vindictus',
+        rating:5,
+        duration:'0:28',
+        cover:'  ' 
+    },
+	 {
+        mp3:'   ',
+        title:'bgm_es6_monster',
+        artist:'Vindictus',
+        rating:5,
+        duration:'0:27',
+        cover:'  ' 
+    },
+	 {
+        mp3:'   ',
+        title:'Keaghan versus Shakarr',
+        artist:'Vindictus',
+        rating:5,
+        duration:'0:11',
+        cover:'  ' 
+    },
+	 {
+        mp3:'   ',
+        title:'bgm_s2ep4_annubin',
+        artist:'Vindictus',
+        rating:5,
+        duration:'0:10',
+        cover:'  ' 
+    },
+	 {
+        mp3:'   ',
+        title:'Heart Beat',
+        artist:'Vindictus',
+        rating:5,
+        duration:'0:02',
         cover:'  ' 
     }
 ];
