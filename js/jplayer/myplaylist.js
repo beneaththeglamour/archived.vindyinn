9465cc8d92e4088c5164cb9558c22e3a711b8854
vindictusinn <saniteeandduros@gmail.com>
2016-08-15 19:58:57 -0700

    Uploading keaghan

--- js/jplayer/myplaylist.js
+++ js/jplayer/myplaylist.js
@@ -9,12 +9,11 @@ var myPlaylist = [
         cover:'http://i.imgur.com/EtuW0tc.jpg'
     },
 	 {
-        mp3:'mix/1.mp3',
-        oga:'mix/1.ogg',
-        title:'Next One',
-        artist:'Sample',
+        mp3:'https://a.tumblr.com/tumblr_obzeenFlj41vdwdpyo1.mp3',
+        title:'Keaghan (Ending)',
+        artist:'Vindictus',
         rating:5,
-        duration:'0:30',
-        cover:'mix/1.png'
+        duration:'3:19',
+        cover:'blob:http://imgur.com/cc228853-2fff-4be5-aefe-019bd55e6caa'
     }
 ];
