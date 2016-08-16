734a742b2f5c7d1d16bca771c2d3b0e5fba6f1ea
vindictusinn <saniteeandduros@gmail.com>
2016-08-16 10:46:07 -0700

    Uploading more

--- js/jplayer/myplaylist.js
+++ js/jplayer/myplaylist.js
@@ -23,5 +23,109 @@ var myPlaylist = [
         rating:5,
         duration:'3:03',
         cover:'http://i.imgur.com/aJGGsJq.jpg' 
+    },
+	 {
+        mp3:'https://a.tumblr.com/tumblr_obzkoxOaYA1vdwdpyo1.mp3',
+        title:'bgm_elcu_end',
+        artist:'Vindictus',
+        rating:5,
+        duration:'2:50',
+        cover:'  ' 
+    },
+	 {
+        mp3:'https://a.tumblr.com/tumblr_oc0jji6QcV1vdwdpyo1.mp3',
+        title:'bgm_mboss_goblin',
+        artist:'Vindictus',
+        rating:5,
+        duration:'2:38',
+        cover:'  ' 
+    },
+	 {
+        mp3:'https://a.tumblr.com/tumblr_oc0jm6Jifw1vdwdpyo1.mp3',
+        title:'bgm_pvp_p02',
+        artist:'Vindictus',
+        rating:5,
+        duration:'2:30',
+        cover:'  ' 
+    },
+	 {
+        mp3:'https://a.tumblr.com/tumblr_oc0jp32DdT1vdwdpyo1.mp3',
+        title:'Succubus',
+        artist:'Vindictus',
+        rating:5,
+        duration:'2:28',
+        cover:'  ' 
+    },
+	 {
+        mp3:'https://a.tumblr.com/tumblr_oc0jt1uen31vdwdpyo1.mp3',
+        title:'bgm_pvp_p01',
+        artist:'Vindictus',
+        rating:5,
+        duration:'2:28',
+        cover:'  ' 
+    },
+	 {
+        mp3:'  ',
+        title:'bgm_s3_braha',
+        artist:'Vindictus',
+        rating:5,
+        duration:'2:21',
+        cover:'  ' 
+    },
+	 {
+        mp3:'  ',
+        title:'bgm_ep7_lizardman',
+        artist:'Vindictus',
+        rating:5,
+        duration:'2:19',
+        cover:'  ' 
+    },
+	 {
+        mp3:'  ',
+        title:'bgm_ep8_ahglan',
+        artist:'Vindictus',
+        rating:5,
+        duration:'2:18',
+        cover:'  ' 
+    },
+	 {
+        mp3:'  ',
+        title:'bgm_ep8_mankind',
+        artist:'Vindictus',
+        rating:5,
+        duration:'2:16',
+        cover:'  ' 
+    },
+	 {
+        mp3:'  ',
+        title:'bgm_ep10_ancient_dragon',
+        artist:'Vindictus',
+        rating:5,
+        duration:'2:15',
+        cover:'  ' 
+    },
+	 {
+        mp3:'  ',
+        title:'bgm_ep10_kadan_prev',
+        artist:'Vindictus',
+        rating:5,
+        duration:'2:15',
+        cover:'  ' 
+    },
+	 {
+        mp3:'  ',
+        title:'bgm_ep10_kadan',
+        artist:'Vindictus',
+        rating:5,
+        duration:'2:12',
+        cover:'  ' 
+    },
+	 {
+        mp3:'  ',
+        title:'bgm_s2ep1_tomb',
+        artist:'Vindictus',
+        rating:5,
+        duration:'2:12',
+        cover:'  ' 
     }
 ];
