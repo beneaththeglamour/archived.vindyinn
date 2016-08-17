e335c0c1ef19839a563656e67a44d17af9a426b3
vindictusinn <saniteeandduros@gmail.com>
2016-08-16 18:21:40 -0700

    Adding tips

---
+++ js/strategiesinfo.js
@@ -0,0 +1,56 @@
+BattleTips = function(){
+	StrategiesName = $('.battlename').html()
+if (StrategiesName == 'Agony and Despair') {$('.strategies').html(''+
+	'<h3>Break-Off Tips</h3>'+
+	'<strong>Location: Chest</strong>'+
+	'<ul>'+
+		'<li>Number of hits: 1 (3 Chain Hooks are required; 1 Rusted Chain Hook is required)</li>'+
+		'<li>Reward: Random Eochaid Armor or Weapon Essence</li>'+
+	'</ul>'+
+	'<h3>Battle Skills & Attacks</h3>'+
+	'<ul>'+
+		'<li>Does one swipe in front of it.</li>'+
+		'<li>Does two swipes in succession. The second swipe will be a lunging attack, causing damage to any player that is close with its body.</li>'+
+		'<li>Charges at a player. The distance is very short so Eochaid can immediately follow up with another attack.</li>'+
+		'<li>Thrashes its body around, damaging anyone that is close to it.</li>'+
+		'<li>Has giant boils on its back, bursting and throwing several projectiles in front of it. Eochaid will be slightly stunned after this attack.'+
+			'<ul>'+
+				'<li>After the projectiles hit either players or the ground, it will spawn miniature infected mobs, which acts like bombs that will track players. </li>'+
+			'</ul>'+
+		'</li>'+
+		'<li>Whenever Eochaid hits 8 bars, the parasite in its chest will take over its body, causing all of its attacks to gain special properties.'+
+			'<ul>'+
+				'<li>At this point, Eochaid will start fighting on its hind legs.</li>'+
+				'<li>In this state, Eochaid will be immune to any SP lockdowns or flinches.</li>'+
+			'</ul>'+
+		'</li>'+
+		'<li>Flashes red, leaps at a player, causing an AoE when he lands. Eochaid will only use this attack whenever it\'s moving towards the center of the room to leap to the top of the room.</li>'+
+		'<li>Walks to the center of the room in order to leap up to the top of the room in order to start raining down ceiling pieces from above.'+
+			'<ul>'+
+				'<li>At this point, players can follow him and grab ahold of its mane in order to get the Rusted Chain Hook and simultaneously avoiding the ceiling pieces falling.</li>'+
+			'</ul>'+
+		'</li>'+
+		'<li>Roars at a player as a taunt. This does not deal any damage to players but it will give an opening for players to chain hook and rip out the parasite from its chest.</li>'+
+		'<li>Does a 4 hit combo, consisting of a stomp, then a kick and then following up with a red-unblockable arm smash and swipe at the end.</li>'+
+		'<li>Does a quick swipe right or left, depending on where players are in its blind spot.</li>'+
+		'<li>Roars, then shifts onto its 4 legs, and then uses the parasite in its chest to fire a red wide cone wind attack.'+
+			'<ul>'+
+				'<li>Players hit with this attack will be inflicted with a debuff called Infected Spore. This debuff will cause 60 damage per second for 15 seconds and decreases your movement speed by 10%, increasing the stack if players are hit continuously with Eochaid\'s roar.</li>'+
+			'</ul>'+
+		'</li>'+
+		'<li>Jumps back and does its red wide cone wind attack.</li>'+
+		'<li>Leaps at a player and lunge-swipe with its body.</li>'+
+		'<li>Does a 5 hit combo consisting of two swipes of its claws in front of it, then a kick, then an arm smash and swipe at the end.</li>'+
+		'<li>Flashes red and leaps at a player.</li>'+
+		'<li>Whenever players manage to rip out the parasite from Eochaid\'s chest, it will revert back to normal, although another parasite will simply appear in its chest again.</li>'+
+		'<li>Does a red roar and spins around, catching any unsuspecting player with its tail for an instant kill.</li>'+
+		'<li>If players are far away from Eochaid, it will launch multiple projectiles at them with its tail.</li>'+
+		'<li>Whenever Eochaid\'s health hits 3 bars, the infection will grow stronger and causes Eochaid\'s attacks to hit harder.'+
+			'<ul>'+
+				'<li>At this point, chain hooks in the corners becomes available for players to use.</li>'+
+			'</ul>'+
+		'</li>'+
+		'<li>At one bar of health left, Eochaid will regain its senses and revert back to normal but at this part, pieces of the ceiling will continuously fall down from above.</li>'+
+	'</ul>'
+)}
+}
